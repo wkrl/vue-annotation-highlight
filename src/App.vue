f81f1197241fffa733f001c0e1c07fda77692600
wkrl <kwxrl@yandex.com>
2020-04-16 13:33:12 +0200

    Improve readability

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,8 @@ export default {
     return {
       text: 'An annotation is extra information associated with a particular point in a document or other piece of information.',
       annotations: [
-        {begin: 3, length: 10, source:'PRED', confidence:'0.9'},
-        {begin: 53, length: 16, source:'PRED', confidence:'0.7'}
+        { begin: 3, length: 10, source:'PRED', confidence:'0.9' },
+        { begin: 53, length: 16, source:'PRED', confidence:'0.7' }
       ]
     }
   },
