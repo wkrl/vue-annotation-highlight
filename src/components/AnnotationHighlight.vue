fcc28f8e284fc68704d3fbaa7c6604f6f401251b
wkrl <kwxrl@yandex.com>
2020-04-16 11:41:37 +0200

    Move getTextAsChunks to /utils

--- src/components/AnnotationHighlight.vue
+++ src/components/AnnotationHighlight.vue
@@ -16,6 +16,7 @@
 
 <script>
 import { annotationValidator } from '../utils/AnnotationValidator'
+import { chunkify } from '../utils/TextToChunks'
 export default {
   name: 'annotation-highlight',
 	props: {
@@ -43,41 +44,42 @@ export default {
 
 	computed: {
     getTextAsChunks() {
-      let chunks = []
-      let unhighlightText = ''
-      for (let offset = 0; offset < this.$props.text.length; offset++) {
-        let annotation = this.getAnnotationByOffset(this.$props.annotations, offset)
-        if (annotation) {
-          if (unhighlightText) chunks.push({text: unhighlightText, isHighlight: false})
-          unhighlightText = ''
-          let highlightText = this.getHighlightByAnnotationLength(offset, annotation.length)
-          chunks.push({text: highlightText, isHighlight: true, annotation: annotation})
-          offset += annotation.length
-        }
-        unhighlightText += this.$props.text.charAt(offset)
-      }
-      if (unhighlightText) chunks.push({text: unhighlightText, isHighlight: false})
-      return chunks
+      return chunkify(this.$props.text, this.$props.annotations)
+      // let chunks = []
+      // let unhighlightText = ''
+      // for (let offset = 0; offset < this.$props.text.length; offset++) {
+      //   let annotation = this.getAnnotationByOffset(this.$props.annotations, offset)
+      //   if (annotation) {
+      //     if (unhighlightText) chunks.push({text: unhighlightText, isHighlight: false})
+      //     unhighlightText = ''
+      //     let highlightText = this.getHighlightByAnnotationLength(offset, annotation.length)
+      //     chunks.push({text: highlightText, isHighlight: true, annotation: annotation})
+      //     offset += annotation.length
+      //   }
+      //   unhighlightText += this.$props.text.charAt(offset)
+      // }
+      // if (unhighlightText) chunks.push({text: unhighlightText, isHighlight: false})
+      // return chunks
     }
   },
 
   methods: {
-    getAnnotationByOffset(annotations, offset) {
-      if (Array.isArray(annotations)) {
-        for (let annotation of annotations) if (annotation.begin == offset) return annotation
-      } else if (annotations.begin == offset) {
-        return annotations
-      }
-      return null
-    },
+    // getAnnotationByOffset(annotations, offset) {
+    //   if (Array.isArray(annotations)) {
+    //     for (let annotation of annotations) if (annotation.begin == offset) return annotation
+    //   } else if (annotations.begin == offset) {
+    //     return annotations
+    //   }
+    //   return null
+    // },
 
-    getHighlightByAnnotationLength(offset, length) {
-      let word = ''
-      for (let i = 0; i < length; i++) {
-        word += this.$props.text.charAt(offset+i)
-      }
-      return word
-		},
+    // getHighlightByAnnotationLength(offset, length) {
+    //   let word = ''
+    //   for (let i = 0; i < length; i++) {
+    //     word += this.$props.text.charAt(offset+i)
+    //   }
+    //   return word
+		// },
   }
 }
 </script>
---
+++ src/utils/TextToChunks.js
@@ -0,0 +1,37 @@
+let chunks = []
+let unhighlightText = ''
+
+const chunkify = (text, annotations) => {
+    for (let offset = 0; offset < text.length; offset++) {
+        let annotation = getAnnotationByOffset(annotations, offset)
+        if (annotation) {
+            let highlightText = text.slice(offset, offset + annotation.length)
+            pushAnnotationChunk(highlightText, annotation)
+            offset += annotation.length
+        }
+        unhighlightText += text.charAt(offset)
+    }
+    if (unhighlightText) pushUnhighlightChunk()
+    return chunks
+}
+
+const getAnnotationByOffset = (annotations, offset) => {
+    if (Array.isArray(annotations)) {
+        for (let annotation of annotations) if (annotation.begin == offset) return annotation
+    } else if (annotations.begin == offset) {
+        return annotations
+    }
+    return null
+}
+
+const pushAnnotationChunk = (highlightText, annotation) => {
+    if (unhighlightText) pushUnhighlightChunk()    
+    chunks.push({text: highlightText, isHighlight: true, annotation: annotation})
+}
+
+const pushUnhighlightChunk = () => {
+    if (unhighlightText) chunks.push({text: unhighlightText, isHighlight: false})
+    unhighlightText = ''
+}
+
+export { chunkify }
