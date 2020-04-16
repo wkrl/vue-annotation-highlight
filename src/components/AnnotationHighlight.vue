4acee71bf121a4ce0bd360da88addd96bbf0d484
wkrl <kwxrl@yandex.com>
2020-04-16 11:46:33 +0200

    Add build

--- src/components/AnnotationHighlight.vue
+++ src/components/AnnotationHighlight.vue
@@ -45,41 +45,7 @@ export default {
 	computed: {
     getTextAsChunks() {
       return chunkify(this.$props.text, this.$props.annotations)
-      // let chunks = []
-      // let unhighlightText = ''
-      // for (let offset = 0; offset < this.$props.text.length; offset++) {
-      //   let annotation = this.getAnnotationByOffset(this.$props.annotations, offset)
-      //   if (annotation) {
-      //     if (unhighlightText) chunks.push({text: unhighlightText, isHighlight: false})
-      //     unhighlightText = ''
-      //     let highlightText = this.getHighlightByAnnotationLength(offset, annotation.length)
-      //     chunks.push({text: highlightText, isHighlight: true, annotation: annotation})
-      //     offset += annotation.length
-      //   }
-      //   unhighlightText += this.$props.text.charAt(offset)
-      // }
-      // if (unhighlightText) chunks.push({text: unhighlightText, isHighlight: false})
-      // return chunks
     }
-  },
-
-  methods: {
-    // getAnnotationByOffset(annotations, offset) {
-    //   if (Array.isArray(annotations)) {
-    //     for (let annotation of annotations) if (annotation.begin == offset) return annotation
-    //   } else if (annotations.begin == offset) {
-    //     return annotations
-    //   }
-    //   return null
-    // },
-
-    // getHighlightByAnnotationLength(offset, length) {
-    //   let word = ''
-    //   for (let i = 0; i < length; i++) {
-    //     word += this.$props.text.charAt(offset+i)
-    //   }
-    //   return word
-		// },
   }
 }
 </script>
