24dc577ba24650d2d6cc4cf503dc3b88b7eb170d
wkrl <kwxrl@yandex.com>
2020-04-16 19:22:25 +0200

    Add component show-case

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <annotation-highlight
-      :text="text"
-      :annotations="annotations"
-    />
+  <div id="app" class="flex flex-col items-center justify-center h-screen m-4">    
+    <div class="text-lg font-bold mb-2 text-gray-900 text-center px-4">annotation-highlight</div>
+    <div class="max-w-md rounded shadow-lg text-left">      
+      <div class="px-6 py-4">
+        <annotation-highlight
+          :text="text"
+          :annotations="annotations"
+          :highlightStyle="isStyleSelected ? highlightStyle : {}"          
+          :highlightComponent="isComponentSelected ? MyHighlightComponent : 'mark'"      
+        />
+      </div>
+      <div class="text-center pb-4 pl-4 pr-4">
+        <label for="highlightstyle" class="pl-2 pr-2 text-sm text-gray-900">highlightStyle</label>
+        <input class="mr-2 leading-tight" id="highlightstyle" type="checkbox" @change="isStyleSelected=!isStyleSelected">
+        <label for="highlightcomponent" class="pl-2 pr-2 text-sm text-gray-900">highlightComponent</label>
+        <input class="mr-2 leading-tight" id="highlightcomponent" type="checkbox" @change="isComponentSelected=!isComponentSelected">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import AnnotationHighlight from './components/AnnotationHighlight'
+import MyHighlightComponent from './components/MyHighlightComponent.vue'
 
 export default {
   data() {
     return {
       text: 'An annotation is extra information associated with a particular point in a document or other piece of information.',
+
       annotations: [
         { begin: 3, length: 10, source:'PRED', confidence:'0.9' },
         { begin: 53, length: 16, source:'PRED', confidence:'0.7' }
-      ]
+      ],
+
+      highlightStyle: {
+        backgroundColor: 'lightblue', 
+        border: '1px solid lightblue',        
+        borderRadius: '4px'
+      },
+      MyHighlightComponent: MyHighlightComponent,
+      
+      isStyleSelected: false,   
+      isComponentSelected: false
     }
   },
 
   components: {
-    AnnotationHighlight,
+    AnnotationHighlight
   }
 }
 </script>
--- src/components/MyHighlightComponent.vue
+++ src/components/MyHighlightComponent.vue
@@ -1,5 +1,55 @@
 <template>
-  <strong>
-    <slot></slot>
-  </strong>
+  <span class="tooltip" @mouseover="hover = true" @mouseleave="hover = false">
+    <ins>
+      <slot></slot>
+    </ins>
+    <div class="tooltiptext" v-if="hover">
+      <p>{{'confidence: ' + annotation.confidence}}</p>
+      <p>{{'source: ' + annotation.source}}</p>
+    </div>
+  </span>
 </template>
+
+<script>
+export default { 
+  props: {
+    annotation: [Object, Array]
+  },
+
+  data() {
+    return {
+      hover: false
+    }    
+  }
+}
+</script>
+
+<style scoped>
+.tooltip {
+  position: relative;
+  display: inline-block;		
+}
+
+.tooltiptext {
+  position: absolute;
+  z-index: 1;	
+  width: 200px;
+  top: 100%;
+  left: 50%;
+  margin-left: -100px; /* -width/2 to center tooltip */
+  background-color: yellow;
+  padding: 8px; 
+  border-radius: 4px;	
+}
+
+.tooltiptext::after {
+  content: " ";
+  position: absolute;
+  bottom: 100%;  /* At the top of the tooltip */
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent yellow transparent;
+}
+</style>
